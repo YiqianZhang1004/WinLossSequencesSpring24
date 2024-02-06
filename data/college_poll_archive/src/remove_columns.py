f63d6663c79f7da4dfb862298d861e75d160749f
samlam00 <sel8@illinois.edu>
2024-02-06 11:17:40 -0600

    added processed_data folder

--- data/college_poll_archive/src/remove_columns.py
+++ data/college_poll_archive/src/remove_columns.py
@@ -1,1 +1,7 @@
 import pandas as pd
+cfb = pd.read_csv("combined_cfb_poll_data.csv")
+cbb = pd.read_csv("combined_cbb_poll_data.csv")
+cfb_trim = cfb[["Date","Rank","Team (FPV)","TeamID","Points"]]
+cbb_trim = cbb[["Date","Rank","Team (FPV)","TeamID","Points"]]
+cfb_trim.to_csv("cfb_polls_trim.csv")
+cbb_trim.to_csv("cbb_polls_trim.csv")
