e4baf5355b87f1aba1f77d3b17e3d4eb2a7161b1
samlam00 <sel8@illinois.edu>
2024-02-07 15:13:40 -0600

    removed unranked data points

--- data/college_poll_archive/src/remove_columns.py
+++ data/college_poll_archive/src/remove_columns.py
@@ -3,7 +3,7 @@ cfb = pd.read_csv("combined_cfb_poll_data.csv")
 cbb = pd.read_csv("combined_cbb_poll_data.csv")
 cfb2 = cfb[["Date","Rank","Team (FPV)","TeamID","Points"]]
 cbb2 = cbb[["Date","Rank","Team (FPV)","TeamID","Points"]]
-cfb_trim = cfb2[cfb.Rank != "NR"]
-cbb_trim = cbb2[cbb.Rank != "RV"]
+cfb_trim = cfb2[cfb.Rank != "RV"]
+cbb_trim = cbb2[cbb.Rank != "NR"]
 cfb_trim.to_csv("cfb_polls_trim.csv")
 cbb_trim.to_csv("cbb_polls_trim.csv")
