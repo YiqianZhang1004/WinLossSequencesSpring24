06e29c5c3f0709d900dbdef7eee6fbc482ebfd9f
David Xia <davidxia143@gmail.com>
2024-02-08 19:24:05 -0600

    added some manual team matching id

--- data/team_dictionary/src/team_dictionary.py
+++ data/team_dictionary/src/team_dictionary.py
@@ -2,77 +2,47 @@ import csv
 import re
 import json
 
-seasons = []
-for i in range(1980, 2024):
-    seasons.append(i)
-
-def get_away_index(season):
-    if season == 2009:
-        return 26
-    elif season == 2023 or season <= 2001:
-        return 22
-    else:
-        return 25
+def extract_capital_letters(input_string):
+    capital_letters = ""
+    for char in input_string:
+        if char.isupper():
+            capital_letters += char
+    return capital_letters
 
 teams = []
 team_id=[]
 
-for season in seasons:
-    with open("data/cfbd/raw_data/sorted_games/sorted_"+str(season)+"_games.csv", 'r') as games_file:
-        games = list(csv.reader(games_file))
-    
-    for game in games:
-        team1 = game[13]
-        team2 = game[get_away_index(season)]
-        team1ID = game[12]
-        team2ID = game[get_away_index(season)-1]
-        if team1 not in teams:
-            teams.append(team1)
-            team_id.append(team1ID)
-        if team2 not in teams:
-            teams.append(team2)
-            team_id.append(team2ID)
+with open("data/cfbd/processed_data/cfbd.csv", 'r') as games_file:
+    games = list(csv.reader(games_file))
+
+for game in games:
+    team1 = game[4]
+    team2 = game[6]
+    team1ID = game[5]
+    team2ID = game[7]
+    if team1 not in teams:
+        teams.append(team1)
+        team_id.append(team1ID)
+    if team2 not in teams:
+        teams.append(team2)
+        team_id.append(team2ID)
 
 
 
 potential_names = {}
 
-for i in range(len(teams)):
-    potential = []
+for i in range(2, len(teams)):
+    potential=[]
     team = str(teams[i]).strip()
-    team = team.replace("&","")
-
     potential.append(team.lower())
-    if team.find(" ")!= -1:
+    if team.find(" ")!=-1:
         potential.append(team.replace(" ","").lower())
-
-    potential.append((team+"u").lower())
-
-    potential.append(team.replace(" State","st").lower())
-
-    potential.append(team.replace("Northern ", "no").lower())
-    potential.append(team.replace("Southern ", "so").lower())
-    potential.append(team.replace("i'i",'ii').lower())
-
-    potential.append(team.replace("Miami (OH)", "MiamiOhio").lower())
-    potential.append(team.replace("Miami","MiamiFlorida").lower())
-
-    
-    pattern = r"\([^()]*\)"
-
-    without = re.sub(pattern, "",team)
-
-    potential.append(without.lower())
-
-    potential.append(without.replace(" ","").lower())
-
-    capital_letters = [char for char in team if char.isupper()]
-
-    potential.append(("".join(capital_letters)).lower())
-    capital_letters = [char for char in without if char.isupper()]
-
-    potential.append(("".join(capital_letters)).lower())
-
+    if len(extract_capital_letters(team))>1:
+        potential.append(extract_capital_letters(team).lower())
+    if team.find(" State")!=-1:
+        potential.append(team.replace(" State", "st").lower())
+        potential.append(team.replace("State","st").lower())
+    potential.append((team+"U").lower())
     potential_names[team_id[i]] = potential
 
 with open("data/team_dictionary/processed_data/cfbd_id_to_potential.json", "w") as json_file:
@@ -82,7 +52,8 @@ with open("data/sportsbookreviewsonline/ncaaf/processsed_data/sbro_ncaaf.csv", "
     sbro = list(csv.reader(file))
 
 sbro_ncaaf_teams = []
-for game in sbro:
+for i in range(1, len(sbro)):
+    game = sbro[i]
     team1 = game[3]
     team2 = game[4]
     if team1 not in sbro_ncaaf_teams:
@@ -98,10 +69,43 @@ for team in sbro_ncaaf_teams:
     for key, value in potential_names.items():
         if team.lower() in value:
             found=True
-            found_teams[key] = team
+            found_teams[team] = int(key)
             break
     if not found:
         unfound.append(team)
+
+
+manual = {}
+manual["MiamiOhio"] = 193
+manual["ULMonroe"] = 2433
+manual["MiamiFlorida"] = 2390
+manual["NoIllinois"] = 2459
+manual["SanJoseState"] = 23
+manual["FloridaAM"] = 50
+manual["FloridaIntl"] = 2229
+manual["ULLafayette"] = 322
+manual["MiddleTennSt"] = 2393
+manual["Hawaii"] = 62
+manual["GardnerWebb"] =2241
+manual["Tenn.Martin"] = 2630
+manual["NichollsSt"] = 2447
+manual["LongIsland"] = 2341
+manual["TexasAM"] = 245
+manual["Cal.PolySLO"] = 13
+manual["TexasA&MCommerce"] = 2837
+manual["SamHoustonSt"] = 2534
+manual["AlabamaAM"] = 2010
+manual["ArkansasPineBluff"] = 2029
+manual["UT-Chattanooga"] = 236
+manual["St.Francis(PA)" ] = 2598
+manual["NO.Colorado"] =2458
+manual["EastTennesseeSt"] = 2193
+
+for team in unfound:
+    if team in manual:
+        unfound.remove(team)
+
+found_teams.update(manual)
 
 with open("data/team_dictionary/processed_data/cfbd_id_to_sbro.json", "w") as file:
     json.dump(found_teams, file, indent=4)
