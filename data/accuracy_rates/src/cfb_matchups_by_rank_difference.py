d1eff7bd0f635a14d65ceade6d8087876efe785e
samlam00 <sel8@illinois.edu>
2024-02-28 14:49:49 -0600

    grouped rank differences by 2, new groupings of teams

--- data/accuracy_rates/src/cfb_matchups_by_rank_difference.py
+++ data/accuracy_rates/src/cfb_matchups_by_rank_difference.py
@@ -26,14 +26,23 @@ def matchups_by_rank_difference(hrl,hru,lwl,lru):
             dictList1.append({"rankDiff":rankDiff,"wins":wins,"games":games})
     df1 = pd.DataFrame(dictList1)
     grouped1 = df1.groupby("rankDiff").agg("sum").reset_index()
-    grouped1["winPct"] = grouped1["wins"] / grouped1["games"]
-
+    print(grouped1)
+    wins1list = grouped1["wins"].to_list()
+    games1list = grouped1["games"].to_list()
+    data1rows=[]
+    for i in range(5):
+        wins1rows = wins1list[2*i] + wins1list[2*i+1]
+        games1rows = games1list[2*i] + games1list[2*i+1]
+        winPct1rows = wins1rows / games1rows
+        data1rows.append({"rankDiff":"".join(str([2*i+1,2*i+2])),"winsHome":wins1rows,"gamesHome":games1rows,"winPctHome":winPct1rows})
+        df1_2rows = pd.DataFrame(data1rows)
+    
     #if away team is ranked i and home team is ranked j
     dictList2 = []
     for i in range (highRankLower,highRankUpper + 1):
-        data_i = data[data.homeRank == i]
+        data_i = data[data.awayRank == i]
         for j in range(lowRankLower,lowRankUpper + 1):
-            data_ij = data_i[data_i.awayRank == j]
+            data_ij = data_i[data_i.homeRank == j]
             if (j > i):
                 rankDiff = j-i
                 games = len(data_ij)
@@ -43,12 +52,33 @@ def matchups_by_rank_difference(hrl,hru,lwl,lru):
             dictList2.append({"rankDiff":rankDiff,"wins":wins,"games":games})
     df2 = pd.DataFrame(dictList2)
     grouped2 = df2.groupby("rankDiff").agg("sum").reset_index()
-    grouped2["winPct"] = grouped2["wins"] / grouped2["games"]
+    print(grouped2)
+    wins2list = grouped2["wins"].to_list()
+    games2list = grouped2["games"].to_list()
+    data2rows=[]
+    for i in range(5):
+        wins2rows = wins2list[2*i] + wins2list[2*i+1]
+        games2rows = games2list[2*i] + games2list[2*i+1]
+        winPct2rows = wins2rows / games2rows
+        data2rows.append({"rankDiff":"".join(str([2*i+1,2*i+2])),"winsAway":wins2rows,"gamesAway":games2rows,"winPctAway":winPct2rows})
+    df2_2rows = pd.DataFrame(data2rows)
 
-    grouped1.to_csv("data/accuracy_rates/processed_data/by_rank_difference_home" + str(highRankLower) + "-" + str(highRankUpper) + ".csv")
-    grouped2.to_csv("data/accuracy_rates/processed_data/by_rank_difference_away" + str(highRankLower) + "-" + str(highRankUpper) + ".csv")
+    #df1_2rows.to_csv("data/accuracy_rates/processed_data/by_rank_difference_home" + str(highRankLower) + "-" + str(highRankUpper) + ".csv")
+    #df2_2rows.to_csv("data/accuracy_rates/processed_data/by_rank_difference_away" + str(highRankLower) + "-" + str(highRankUpper) + ".csv")
+    print(df1_2rows)
+    print(df2_2rows)
+    df = pd.merge(df1_2rows,df2_2rows,on="rankDiff")
+    df.to_csv("data/accuracy_rates/processed_data/by_rank_difference_combined" + str(hrl) + "-" + str(hru) + ".csv")
 
-matchups_by_rank_difference(1,5,1,25)
-matchups_by_rank_difference(6,10,1,25)
+#matchups_by_rank_difference(1,5,1,25)
+#matchups_by_rank_difference(6,10,1,25)
+#matchups_by_rank_difference(11,15,1,25)
+#matchups_by_rank_difference(1,10,1,25)
+#matchups_by_rank_difference(11,20,1,25)
 matchups_by_rank_difference(15,20,1,25)
-matchups_by_rank_difference(20,25,1,25)
+
+#graphics:
+# top ten teams vs teams ranked x teams below them
+# 11-25 teams vs teams ranked x teams below them
+
+# Combine rows of data frames by rankDiff, for example rankDiff of 3 and 4 in one category
