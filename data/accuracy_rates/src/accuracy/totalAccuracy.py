94455a22c9f015449c865839e05ec9d95ae69a7e
David Xia <davidxia143@gmail.com>
2024-02-28 19:20:54 -0600

    updated bar charts

--- data/accuracy_rates/src/accuracy/totalAccuracy.py
+++ data/accuracy_rates/src/accuracy/totalAccuracy.py
@@ -8,13 +8,17 @@ elo_accuracy = accuracy_rates.get_accuracy("e", seasons, [], [], [], [], "", "")
 moneyline_accuracy = accuracy_rates.get_accuracy("m", seasons, [], [], [], [], "", "")[0]
 poll_accuracy = accuracy_rates.get_accuracy("p", seasons, [], [], [], [], "", "")[0]
 
-categories = ["elo", "moneyline", "poll"]
-values = [elo_accuracy, moneyline_accuracy, poll_accuracy]
+categories = ["Moneyline","Elo" ,"Poll"]
+values = [moneyline_accuracy, elo_accuracy, poll_accuracy]
+colors = ['green', 'blue', 'red']
 
-plt.bar(categories, values, color='blue')
+plt.bar(categories, values, color=colors)
 
 plt.ylabel('Total Accuracy Rate')
 plt.title('Elo, Moneyline, and Poll Total Accuracy Rates of All Teams (2007 - 2019)')
+
+plt.ylim(50, 80)
+
 
 plt.savefig('data/accuracy_rates/visualizations/empTotalFull.png')
 
