9c3a5034f97507479a9d7070108a7b582a69fe39
samlam00 <sel8@illinois.edu>
2024-04-14 16:20:54 -0500

    reupload

--- sports-reference/src/cfb_full_seasons.py
+++ sports-reference/src/cfb_full_seasons.py
@@ -9,7 +9,7 @@ driver = webdriver.Edge(options= edge_options)
 
 data = []
 # 1872-2024
-for i in range(2017,2024):
+for i in range(1980,2024):
     print(i)
     driver.get("https://www.sports-reference.com/cfb/years/"+str(i)+"-schedule.html")
     time.sleep(0.5)
@@ -24,10 +24,13 @@ for i in range(2017,2024):
             row = rows[k].find_elements(By.CSS_SELECTOR,"td")
             if len(row) == 0:
                 continue
-            d = {"date":row[1].text,"winner":row[4].text,"location":row[6].text,"loser":row[7].text}
+            if (i>=2013):
+                d = {"date":row[1].text,"winner":row[4].text,"location":row[6].text,"loser":row[7].text}
+            else:
+                d = {"date":row[1].text,"winner":row[3].text,"location":row[5].text,"loser":row[6].text}
             data.append(d)
     except:
         print("except")
         continue
 df = pd.DataFrame(data)
-print(df)
+df.to_csv("sports-reference/raw_data/cfb_seasons.csv")
